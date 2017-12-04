e13d91b626a8515346e43e3d237ab499dd54ced8
JOSE LUIS BUSTOS ESTEBAN <jlbustos@equilibrha.es>
2017-12-04 21:49:27 +0100

    init

--- Lab1Integration/Lab1IntegrationTests/Lab1IntegrationTests.swift
+++ Lab1Integration/Lab1IntegrationTests/Lab1IntegrationTests.swift
@@ -24,6 +24,7 @@ class Lab1IntegrationTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true) //añadimos este test
     }
     
     func testPerformanceExample() {
